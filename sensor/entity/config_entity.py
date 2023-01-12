66f0068cc32c5a83b7dba71b5a81b73be6a141c5
Bhupendra1770 <bhupendra360i@gmail.com>
2023-01-12 16:06:10 +0530

    config_entity.py

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -45,7 +45,7 @@ class DataValidationConfig:
         self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_validation")
         self.report_file_path=os.path.join(self.data_validation_dir, "report.yaml")
         self.missing_threshold:float = 0.2
-        self.base_file_path = os.path.join("aps_failure_training_set1.csv")
+        self.base_file_path = os.path.join("adult.csv")
 
 class DataTransformationConfig:
 
