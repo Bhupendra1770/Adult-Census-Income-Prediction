c66d7583ac07345fe11257cca42e22003735d489
Bhupendra1770 <bhupendra360i@gmail.com>
2023-01-26 23:01:10 +0530

    s3

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -8,10 +8,10 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    'sensor_training',
+    'census_training',
     default_args={'retries': 2},
     # [END default_args]
-    description='Sensor Fault Detection',
+    description='Adult census income prediction',
     schedule_interval="@weekly",
     start_date=pendulum.datetime(2022, 12, 11, tz="UTC"),
     catchup=False,
